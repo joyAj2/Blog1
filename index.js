0245f6ca54e04d61e8fe1efe42782d84eea7e513
joyAj2 <joyejeh0@gmail.com>
2024-03-04 21:04:39 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const posts = [];
+
+// Function to generate a unique ID for posts
+function generateUniqueId() {
+  return Date.now();
+}
+
+// Set up middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+
+// Render the index page with posts
+app.get("/", (req, res) => {
+  res.render("index.ejs", { posts: posts });
+});
+
+// Render the about page
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+// Render the contact page
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+// Render the form to create a new post
+app.get("/posts/new", (req, res) => {
+  res.render("create-post.ejs");
+});
+
+// Create a new post
+app.post("/create-post", (req, res) => {
+  const { title, content } = req.body;
+  const postId = generateUniqueId(); // Generate a unique ID for the post
+  const newPost = { id: postId, title, content };
+  posts.push(newPost); // Add the new post to the posts array
+  res.redirect("/"); // Redirect back to the index page
+});
+
+// Render the edit form for a specific post
+app.get("/edit/:id", (req, res) => {
+  const itemId = req.params.id;
+  const item = posts.find((post) => post.id === parseInt(itemId));
+
+  if (!item) {
+    return res.status(404).send("Post not found");
+  }
+
+  res.render("edit-form.ejs", { item });
+});
+
+// Update a specific post
+app.post("/update/:id", (req, res) => {
+  const itemId = req.params.id;
+  const { title, content } = req.body;
+
+  const index = posts.findIndex((post) => post.id === parseInt(itemId));
+  if (index !== -1) {
+    posts[index].title = title;
+    posts[index].content = content;
+  }
+
+  res.redirect("/");
+});
+
+// Delete a specific post
+app.post("/posts/:id/delete", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const index = posts.findIndex((post) => post.id === postId);
+  if (index !== -1) {
+    posts.splice(index, 1);
+    res.redirect("/"); // Redirect back to the index page after deletion
+  } else {
+    res.status(404).send("Post not found");
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
